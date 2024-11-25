d425c5b47e3be90030f92ca637af3f7e30f7e130
Adilet Baryktabasov <75193190+Adilet25@users.noreply.github.com>
2024-11-25 22:39:34 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_7KPbdDpce', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3saoiv516hmfal1dtm9lrk8vv1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Ug4S5bL9K', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '39p2pniocoddef38332ejl4e0u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
